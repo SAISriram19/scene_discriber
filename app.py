23423d3da85cef76b6664ef4965a149d91ae99ec
sai sriram reddy <127861110+SAISriram19@users.noreply.github.com>
2024-11-01 22:19:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import streamlit as st
 from main import SceneSpeak
+from PIL import Image
+import io
 
 scene_speak = SceneSpeak()
 
-st.title("Scene Speak")
+st.title("Scene Discriber")
 st.write("Upload an image to detect objects and generate a description.")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    image = uploaded_file.read()
+    # Read and display the uploaded image
+    image = Image.open(io.BytesIO(uploaded_file.read()))
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     
     if st.button("Describe Scene"):
-        description = scene_speak.describe_scene(uploaded_file)
+        description = scene_speak.describe_scene(image)
         st.write(description)
